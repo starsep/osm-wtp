fc824213227a8a742f7ed350e4bfc9a3e0a3c691
Filip Czaplicki <github@starsep.com>
2023-01-08 10:48:33 +0100

    Update server script

---
+++ update-server.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+date=$(/bin/date -u '+%Y%m%d')
+touch .dateOfLastRun
+if [[ "$(cat .dateOfLastRun)" != "$date" ]]; then
+    rm -rf cache/wtp
+fi
+git pull
+rm -rf cache/overpass
+source .venv/bin/activate
+pip install -r requirements.txt
+python main.py
+cd ../osm-wtp || return
+git config user.name "OSM WTP Bot"
+git config user.email "<>"
+mv index.html index2.html
+git pull
+mv index2.html index.html
+git add index.html
+git commit -m "Update $date"
+git push origin main
+cd ../osm-wtp-compare || return
+echo "$date" > .dateOfLastRun
