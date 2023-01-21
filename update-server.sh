1436e2069e50fa4f7c8875cb2581149334c960f3
Filip Czaplicki <github@starsep.com>
2023-01-22 00:22:06 +0100

    Download GTFS

--- update-server.sh
+++ update-server.sh
@@ -4,6 +4,15 @@ touch .dateOfLastRun
 if [[ "$(cat .dateOfLastRun)" != "$date" ]]; then
     rm -rf cache
 fi
+mkdir -p ../GTFS-Warsaw
+cd ../GTFS-Warsaw
+OLD_HASH=$(md5sum warsaw.zip)
+wget -c -O warsaw.zip https://mkuran.pl/gtfs/warsaw.zip
+if [[ $(md5sum warsaw.zip) != "$OLD_HASH" ]]; then
+	7z x -y warsaw.zip
+	chmod 0644 -- *
+fi
+cd ../osm-wtp-compare
 git pull
 rm -rf cache/overpass
 source .venv/bin/activate
