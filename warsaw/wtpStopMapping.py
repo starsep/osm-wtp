92c1c906308149c3ff227f43aedffb4a1877f7b0
Paweł Raszuk <pawel.raszuk@gmail.com>
2023-05-24 18:11:16 +0200

    Add stop Os Krolewska 02->52

--- warsaw/wtpStopMapping.py
+++ warsaw/wtpStopMapping.py
@@ -6,4 +6,5 @@ from model.types import StopRef, StopName
 wtpStopMapping: Dict[Tuple[StopRef, StopName], Tuple[StopRef, StopName]] = {
     ("512002", "Kolejowa 02"): ("512052", "Kolejowa 52"),
     ("107002", "Kamienna 02"): ("107052", "Kamienna 52"),
+    ("335302", "Oś Królewska 02"): ("335352", "Oś Królewska 52"),  # line 379
 }
