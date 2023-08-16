3fd52e2b050989d12b85165e69d8f12f3cab63c5
Filip Czaplicki <github@starsep.com>
2023-08-17 00:03:31 +0200

    Set WTP cache expire for 12 hours

--- configuration.py
+++ configuration.py
@@ -5,4 +5,4 @@ MISSING_REF = "-"
 cacheDirectory = Path("cache")
 outputDirectory = Path("osm-wtp")
 OVERPASS_URL = "https://overpass-api.de/api/interpreter"  # "http://localhost:12345/api/interpreter"
-EXPIRE_WTP_SECONDS = 60 * 60 * 24
+EXPIRE_WTP_SECONDS = 60 * 60 * 12
