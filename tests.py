8889010cd6074c0383c32af706880ee96838e871
Filip Czaplicki <github@starsep.com>
2023-08-20 23:08:29 +0200

    Remove outdated lastStopRef test

--- tests.py
+++ tests.py
@@ -1,20 +1,12 @@
 import unittest
 
-from configuration import MISSING_REF
 from distance import geoDistance, GeoPoint
-from warsaw.wtpScraper import lastStopRef, mapWtpStop
+from warsaw.wtpScraper import mapWtpStop
 
 
 class OSMWTPCompareTests(unittest.TestCase):
     def test_mapWtpStop(self):
         self.assertEqual(mapWtpStop("100081", "Test 81"), ("100001", "Test 01"))
-
-
-class LastStopRefTests(unittest.TestCase):
-    def test_lastStopRef(self):
-        self.assertEqual(lastStopRef("Nowa 01", "170201"), "199801")
-        self.assertEqual(lastStopRef("Bródno-Podgrodzie 08", ""), "115208")
-        self.assertEqual(lastStopRef("Example", ""), MISSING_REF)
 
 
 class GeoDistanceTests(unittest.TestCase):
