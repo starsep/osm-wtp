ee7fe5fdeb3195ce503c96a65e7a9d7be6b5fc01
Filip Czaplicki <github@starsep.com>
2024-06-13 17:36:39 +0200

    Don't fail when API UM returns error

--- warsaw/fetchApiRoutes.py
+++ warsaw/fetchApiRoutes.py
@@ -47,11 +47,15 @@ def fetchApiRoutes() -> dict[RouteRef, List[APIUMWarszawaRouteResult]]:
             "Missing API UM Warszawa api key. Set it as API_KEY environment variable"
         )
         return dict()
-    resourceId = "26b9ade1-f5d4-439e-84b4-9af37ab7ebf1"
-    url = f"https://api.um.warszawa.pl/api/action/public_transport_routes/?apikey={API_UM_WARSZAWA_API_KEY}&resource_id={resourceId}"
-    with log_duration("Downloading data from API UM Warszawa"):
-        response = httpx.get(url)
-        response.raise_for_status()
-    with log_duration("Parsing API UM Warszawa JSON"):
-        data = json.loads(response.text)["result"]
-    return _parseApiUMData(data)
+    try:
+        resourceId = "26b9ade1-f5d4-439e-84b4-9af37ab7ebf1"
+        url = f"https://api.um.warszawa.pl/api/action/public_transport_routes/?apikey={API_UM_WARSZAWA_API_KEY}&resource_id={resourceId}"
+        with log_duration("Downloading data from API UM Warszawa"):
+            response = httpx.get(url)
+            response.raise_for_status()
+        with log_duration("Parsing API UM Warszawa JSON"):
+            data = json.loads(response.text)["result"]
+        return _parseApiUMData(data)
+    except Exception as e:
+        logger.error(f"Failed to fetch data from API UM Warszawa: {e}")
+        return dict()
