22297500503ca46d15dc0f92edfba64aecb58e76
Filip Czaplicki <github@starsep.com>
2024-06-15 14:43:10 +0200

    vulture

---
+++ whitelist.py
@@ -0,0 +1,3 @@
+operatorLink  # unused variable (osm/OSMRelationAnalyzer.py:75)
+routeType  # unused variable (osm/OSMRelationAnalyzer.py:83)
+compareApiRoutesWithOSM  # unused function (warsaw/compareApiRoutesWithOSM.py:10)
