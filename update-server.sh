c570e545d7c7bdcce2325efc314f50f2d4d98ec1
Filip Czaplicki <github@starsep.com>
2025-05-10 20:52:30 +0200

    uv run --no-dev --no-progress

--- update-server.sh
+++ update-server.sh
@@ -3,7 +3,7 @@ set -eu
 date=$(/bin/date '+%Y%m%d')
 ./updateGTFSWarsaw.sh
 git clone https://$GITHUB_USERNAME:$GITHUB_TOKEN@github.com/starsep/osm-wtp/ --depth 1 --branch gh-pages
-uv run python main.py
+uv run --no-dev --no-progress python main.py
 (
     cd osm-wtp || exit 1
     git config user.name "OSM WTP Bot"
